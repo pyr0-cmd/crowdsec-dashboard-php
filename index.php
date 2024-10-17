c86011d922324d9ba0c5c0548db6b2a766299492
pyr0-cmd <tainguyen7643@proton.me>
2024-10-18 00:05:40 +0700

    nothing

--- index.php
+++ index.php
@@ -11,17 +11,7 @@
 </head>
 <body class="bg-gray-100 font-sans leading-normal tracking-normal">
     <!-- Navbar -->
-    <nav class="bg-blue-600 p-4">
-        <div class="container mx-auto flex justify-between items-center">
-            <div class="text-white text-xl font-bold">Crowdsec Dashboard</div>
-            <ul class="flex space-x-4">
-                <li><a href="#" class="text-white hover:text-blue-200">Home</a></li>
-                <li><a href="#" class="text-white hover:text-blue-200">Decisions</a></li>
-                <li><a href="#" class="text-white hover:text-blue-200">Alerts</a></li>
-                <li><a href="#" class="text-white hover:text-blue-200">Settings</a></li>
-            </ul>
-        </div>
-    </nav>
+    <?php include 'navbar.php'; ?>
 
     <!-- Main Content -->
     <div class="flex justify-start items-start h-screen p-8">
--- navbar.php
+++ navbar.php
@@ -2,9 +2,9 @@
         <div class="container mx-auto flex justify-between items-center">
             <div class="text-white text-xl font-bold">Crowdsec Dashboard</div>
             <ul class="flex space-x-4">
-                <li><a href="#" class="text-white hover:text-blue-200">Home</a></li>
+                <li><a href="index.php" class="text-white hover:text-blue-200">Home</a></li>
                 <li><a href="#" class="text-white hover:text-blue-200">Decisions</a></li>
-                <li><a href="#" class="text-white hover:text-blue-200">Alerts</a></li>
+                <li><a href="alerts.php" class="text-white hover:text-blue-200">Alerts</a></li>
                 <li><a href="#" class="text-white hover:text-blue-200">Settings</a></li>
             </ul>
         </div>
