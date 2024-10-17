410f975d940419f7ace51ad9a64eff8ec02fdf0f
pyr0-cmd <tainguyen7643@proton.me>
2024-10-17 23:34:47 +0700

    modify decision, alert list

--- alerts.php
+++ alerts.php
@@ -1,5 +1,24 @@
 <?php
-    include 'db_connect/alerts_list.php';
+    include 'db_connect/db.php';
+    $dbconn = connect_db();
+    $query_alerts = 'SELECT * FROM alerts';
+    $rs_alerts = pg_query($dbconn, $query_alerts);
 
-    echo alertList();
+    if (!$rs_alerts) {
+        echo json_encode(['error' => 'Error executing query: ' . pg_last_error()]);
+        pg_close($dbconn);
+        exit();
+    }
+
+    // Fetch data into an array
+    $alerts = array();
+    while ($row = pg_fetch_assoc($rs_alerts)) {
+        $alerts[] = $row;
+    }
+
+    // Free result set and close connection
+    pg_free_result($rs_alerts);
+    pg_close($dbconn);
+    return $alerts;
+    
 ?>
--- decisions.php
+++ decisions.php
@@ -1,8 +1,26 @@
-<?php 
-    include 'db_connect/decisions_list.php';
+<?php
+    include 'db_connect/db.php';
+    function decisionList(){
+        $dbconn = connect_db();
+        $query_decisions = 'SELECT * FROM decisions';
+        $rs_decisions = pg_query($dbconn, $query_decisions);
 
-    $dec = decisionList();
-    $test = json_encode($dec);
+        if (!$rs_decisions) {
+            echo json_encode(['error' => 'Error executing query: ' . pg_last_error()]);
+            pg_close($dbconn);
+            exit();
+        }
 
-    echo $dec;
+        // Fetch data into an array
+        $decisions = array();
+        while ($row = pg_fetch_assoc($rs_decisions)) {
+            $decisions[] = $row;
+        }
+
+        // Free result set and close connection
+        pg_free_result($rs_decisions);
+        pg_close($dbconn);
+
+        return $decisions;
+    }
 ?>
