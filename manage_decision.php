a1f70d9e0e8616de148b96a6136336af68b299f6
pyr0-cmd <tainguyen7643@proton.me>
2024-10-23 08:49:30 +0700

    save manage decision code

---
+++ manage_decision.php
@@ -0,0 +1,47 @@
+<?php 
+    session_start();
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: login.php");
+        exit();
+    }
+
+    include 'db_connect/db.php';
+    $dbconn = connect_db();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['create'])) {
+            $scenario = pg_escape_string($dbconn, $_POST['scenario']);
+            $type = pg_escape_string($dbconn, $POST['type']);
+            $value = pg_escape_string($dbconn, $_POST['value']);
+
+            $origin = 'admin';
+            $scope  = 'Ip';
+
+            $current_time = date('Y-m-d H:i:s');
+
+            $insert_query = "INSERT INTO decisions (origin, scenario, created_at, updated_at, until, type, value, scope) 
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8)";
+            
+            $result = pg_query_params($dbconn, $insert_query, [
+                $origin,
+                $scenario,
+                $current_time,
+                $current_time,
+                $_POST['until'],
+                $type,
+                $value,
+                $scope
+            ]);
+
+            
+            if (!$result) {
+                $error = "Error adding record: " . pg_last_error($dbconn);
+            }
+
+        } elseif (isset($_POST['delete'])) {
+            $id = intval($_POST['id']);
+            $delete_query = "DELETE FROM decisions WHERE id = $1";
+            $result = pg_query_params($dbconn, $delete_query, [$id]);
+        }
+    }
+?>
