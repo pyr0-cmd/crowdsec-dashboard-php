2c1c6ba4cf28a764a0bf2d4be0d60a1e2ec58350
pyr0-md <tainguyen7643@proton.me>
2024-11-06 01:17:44 +0000

    change title

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <nav class="bg-gray-600 fixed h-full md:w-64 w-full z-50 top-0 md:top-0 md:left-0 p-4 shadow-lg">
     <div class="container mx-auto flex flex-col md:space-y-6 space-y-4 items-center md:items-start">
         <div class="text-white text-2xl font-bold mb-6">
-            <a href="index.php" class="hover:text-green-300 transition-colors duration-200">CrowdSec Dashboard</a>
+            <a href="index.php" class="hover:text-green-300 transition-colors duration-200">Trình quản lý Crowdsec</a>
         </div>
         <a href="index.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
             Trang Chủ
@@ -16,7 +16,7 @@
             Cảnh Báo
         </a>
         <a href="manage_decision.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
-            Trình quản lý
+            Quản lý chặn
         </a>
     </div>
 </nav>
