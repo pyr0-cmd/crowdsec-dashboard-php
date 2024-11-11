cde52c5a8c74e1e1225300170ae910f543c68024
pyr0-md <tainguyen7643@proton.me>
2024-11-11 03:42:44 +0000

    modify all structure to MVC model

---
+++ views/navbar.php
@@ -0,0 +1,22 @@
+<nav class="bg-gray-600 fixed h-full md:w-64 w-full z-50 top-0 md:top-0 md:left-0 p-4 shadow-lg">
+    <div class="container mx-auto flex flex-col md:space-y-6 space-y-4 items-center md:items-start">
+        <div class="text-white text-2xl font-bold mb-6">
+            <a href="../index.php" class="hover:text-green-300 transition-colors duration-200">Trình quản lý Crowdsec</a>
+        </div>
+        <a href="../index.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
+            Trang Chủ
+        </a>
+        <a href="../models/decisions.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
+            Danh sách Chặn
+        </a>
+        <a href="../models/machines.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
+            Danh sách Agent
+        </a>
+        <a href="../models/alerts.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
+            Cảnh Báo
+        </a>
+        <a href="../models/manage_decision.php" class="text-white hover:bg-green-700 w-full text-center md:text-left py-2 px-4 rounded-md transition-colors duration-200">
+            Quản lý chặn
+        </a>
+    </div>
+</nav>
